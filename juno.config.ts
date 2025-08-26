44d48cb074197c6d3e9bd68e52ed74f1fb79977a
nami2111 <aimsomnia@proton.me>
2025-08-27 04:40:45 +0900

    Migrate CI/CD workflows to pnpm and update Juno deployment configurations

--- juno.config.ts
+++ juno.config.ts
@@ -1,15 +1,15 @@
-import {defineConfig} from '@junobuild/config';
+import { defineConfig } from '@junobuild/config';
 
 export default defineConfig({
-  satellite: {
-    ids: {
-      development: '<DEV_SATELLITE_ID>',
-      production: 'dpl4s-kqaaa-aaaal-asg3a-cai'
-    },
-    source: 'build',
-    predeploy: ['pnpm build']
-  },
-  orbiter: {
-    id: 'p2pi7-hiaaa-aaaal-asaia-cai'
-  }
+	satellite: {
+		ids: {
+			development: '<DEV_SATELLITE_ID>',
+			production: 'dpl4s-kqaaa-aaaal-asg3a-cai'
+		},
+		source: 'build',
+		predeploy: ['pnpm build']
+	},
+	orbiter: {
+		id: 'p2pi7-hiaaa-aaaal-asaia-cai'
+	}
 });
